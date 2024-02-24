bf4a1c9d833603ffa67697e42d3dcd50cebc0944
Harsh Karande <harshskarande@gmail.com>
2024-02-25 00:33:10 +0530

    create update for tail-file class

--- server/src/tail-file.ts
+++ server/src/tail-file.ts
@@ -3,6 +3,7 @@ import events from 'events'
 
 const INITIAL_READ_LINES = 10
 const READ_CHUNK_SIZE = 8
+const POLLING_INTERVAL = 1000
 
 export default class TailFile extends events.EventEmitter {
   private queue: string[]
@@ -37,7 +38,7 @@ export default class TailFile extends events.EventEmitter {
       stringTail = buffer.toString() + stringTail
 
       const newLineChars = stringTail.match(/\n(?!$)/g)?.length ?? 0
-      if (newLineChars >= INITIAL_READ_LINES) {
+      if (newLineChars >= INITIAL_READ_LINES || position === 0) {
         this.queue = stringTail.split(/\n(?!$)/).slice(-INITIAL_READ_LINES)
         break
       }
@@ -45,5 +46,31 @@ export default class TailFile extends events.EventEmitter {
 
     fs.closeSync(file)
     this.emit('init', this.queue)
+
+    fs.watchFile(this.filename, { interval: POLLING_INTERVAL }, (curr, prev) => {
+      this.update(curr, prev)
+    })
+  }
+
+  update(curr: fs.Stats, prev: fs.Stats) {
+    if (curr.ino !== prev.ino || curr.size < prev.size) {
+      this.eof = 0
+    }
+
+    const file = fs.openSync(this.filename, 'r')
+    const bufferSize = curr.size - this.eof
+    const buffer = Buffer.alloc(bufferSize)
+    fs.readSync(file, buffer, 0, bufferSize, this.eof)
+    fs.closeSync(file)
+
+    const newLines = buffer.toString().split(/\n(?!$)/)
+    if (newLines.length >= INITIAL_READ_LINES) {
+      this.queue = newLines.slice(-INITIAL_READ_LINES)
+    } else {
+      this.queue = this.queue.slice(-INITIAL_READ_LINES + newLines.length).concat(newLines)
+    }
+    this.eof = curr.size
+
+    this.emit('update', newLines)
   }
 }
